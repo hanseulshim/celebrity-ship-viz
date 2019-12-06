e833d46a6befbcab327fd2fdc70e265629057609
Hanseul Shim <hanseulshim@gmail.com>
2019-12-05 22:38:49 -0500

    Add 3d chart and deployment packages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Plot from 'react-plotly.js'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const colors = ['#ED553B', '#20639B', '#CCCCCC']
+const getColor = () => colors[Math.round(Math.random() * 2)]
+
+const data = []
+
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    for (let k = 0; k < 10; k++) {
+      data.push({
+        type: 'mesh3d',
+        color: getColor(),
+        x: [k, k, k + 1, k + 1],
+        y: [j, j + 1, j + 1, j],
+        z: [i, i, i, i]
+      })
+    }
+  }
 }
 
-export default App;
+const App = () => (
+  <Plot
+    data={data}
+    useResizeHandler={true}
+    style={{ width: '100%', height: '1000px' }}
+  />
+)
+
+export default App
