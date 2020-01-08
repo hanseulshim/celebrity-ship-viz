90987abd84236ba6e7229aa1e53f6db5df388f2b
Hanseul Shim <hanseulshim@gmail.com>
2020-01-08 15:33:26 -0500

    Updates to chart

--- src/Playground.js
+++ src/Playground.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import Plot from 'react-plotly.js'
 
+const RED = 'red'
+const YELLOW = 'yellow'
+const GREEN = 'green'
+
 const hoverInfo = {
   hoverlabel: {
     bgcolor: 'rgba(2, 19, 43, 0.7)',
@@ -9,10 +13,8 @@ const hoverInfo = {
       color: '#FFF'
     }
   },
-  // hovertemplate:
-  //   '   Room 8234   <br />   BOOKED   <br />   Max 4   <br />   Booked 2   <br /><br /><extra></extra>'
   hovertemplate:
-    '   Room %{customdata.status}   <br />   BOOKED   <br />   Max 4   <br />   Booked 2   <br /><br /><extra></extra>'
+    '   Room %{customdata.roomNumber}   <br />   BOOKED   <br />   Max %{customdata.maxOccupancy}   <br />   Booked %{customdata.booked}   <br /><br /><extra></extra>'
 }
 
 const data = [
@@ -24,170 +26,170 @@ const data = [
       y1: 2,
       z: 0,
       roomNumber: 5832,
-      status: 'booked',
       maxOccupancy: 4,
       booked: 4
     },
-    // {
-    //   x0: 0,
-    //   x1: 2,
-    //   y0: 2,
-    //   y1: 3,
-    //   z: 0,
-    //   roomNumber: 5833,
-    //   status: 'booked',
-    //   maxOccupancy: 2,
-    //   booked: 1
-    // },
-    // {
-    //   x0: 0,
-    //   x1: 2,
-    //   y0: 3,
-    //   y1: 4,
-    //   z: 0,
-    //   roomNumber: 5834,
-    //   status: 'unbooked',
-    //   maxOccupancy: 2,
-    //   booked: 0
-    // },
-    // {
-    //   x0: 0,
-    //   x1: 2,
-    //   y0: 4,
-    //   y1: 5,
-    //   z: 0,
-    //   roomNumber: 5835,
-    //   status: 'unbooked',
-    //   maxOccupancy: 2,
-    //   booked: 2
-    // },
     {
-      x0: 4,
+      x0: 0,
+      x1: 2,
+      y0: 2,
+      y1: 3,
+      z: 0,
+      roomNumber: 5833,
+      maxOccupancy: 2,
+      booked: 1
+    },
+    {
+      x0: 0,
+      x1: 2,
+      y0: 3,
+      y1: 4,
+      z: 0,
+      roomNumber: 5834,
+      maxOccupancy: 2,
+      booked: 0
+    },
+    {
+      x0: 0,
+      x1: 2,
+      y0: 4,
+      y1: 5,
+      z: 0,
+      roomNumber: 5835,
+      maxOccupancy: 2,
+      booked: 1
+    },
+    {
+      x0: 3,
       x1: 5,
       y0: 0,
       y1: 2,
       z: 0,
+      roomNumber: 5836,
+      maxOccupancy: 2,
+      booked: 2
+    },
+    {
+      x0: 3,
+      x1: 5,
+      y0: 2,
+      y1: 3,
+      z: 0,
+      roomNumber: 5837,
+      maxOccupancy: 2,
+      booked: 0
+    },
+    {
+      x0: 3,
+      x1: 5,
+      y0: 3,
+      y1: 4,
+      z: 0,
+      roomNumber: 5838,
+      maxOccupancy: 2,
+      booked: 1
+    },
+    {
+      x0: 3,
+      x1: 5,
+      y0: 4,
+      y1: 5,
+      z: 0,
+      roomNumber: 5839,
+      maxOccupancy: 4,
+      booked: 4
+    }
+  ],
+  [
+    {
+      x0: 0,
+      x1: 2,
+      y0: 0,
+      y1: 2,
+      z: 1,
       roomNumber: 5832,
-      status: 'unbooked',
+      maxOccupancy: 4,
+      booked: 4
+    },
+    {
+      x0: 0,
+      x1: 2,
+      y0: 2,
+      y1: 3,
+      z: 1,
+      roomNumber: 5833,
+      maxOccupancy: 2,
+      booked: 1
+    },
+    {
+      x0: 0,
+      x1: 2,
+      y0: 3,
+      y1: 4,
+      z: 1,
+      roomNumber: 5834,
+      maxOccupancy: 2,
+      booked: 0
+    },
+    {
+      x0: 0,
+      x1: 2,
+      y0: 4,
+      y1: 5,
+      z: 1,
+      roomNumber: 5835,
       maxOccupancy: 2,
       booked: 2
     }
-    // {
-    //   x0: 3,
-    //   x1: 5,
-    //   y0: 2,
-    //   y1: 3,
-    //   z: 0,
-    //   status: 'unbooked',
-    //   maxOccupancy: 2,
-    //   booked: 0
-    // },
-    // {
-    //   x0: 3,
-    //   x1: 5,
-    //   y0: 3,
-    //   y1: 4,
-    //   z: 0,
-    //   roomNumber: 5833,
-    //   status: 'booked',
-    //   maxOccupancy: 2,
-    //   booked: 1
-    // },
-    // {
-    //   x0: 3,
-    //   x1: 5,
-    //   y0: 4,
-    //   y1: 5,
-    //   z: 0,
-    //   status: 'booked',
-    //   maxOccupancy: 4,
-    //   booked: 4
-    // }
   ]
-  // [
-  //   {
-  //     x0: 0,
-  //     x1: 2,
-  //     y0: 0,
-  //     y1: 2,
-  //     z: 1,
-  //     roomNumber: 5832,
-  //     status: 'booked',
-  //     maxOccupancy: 4,
-  //     booked: 4
-  //   },
-  //   {
-  //     x0: 0,
-  //     x1: 2,
-  //     y0: 2,
-  //     y1: 3,
-  //     z: 1,
-  //     roomNumber: 5833,
-  //     status: 'booked',
-  //     maxOccupancy: 2,
-  //     booked: 1
-  //   },
-  //   {
-  //     x0: 0,
-  //     x1: 2,
-  //     y0: 3,
-  //     y1: 4,
-  //     z: 1,
-  //     roomNumber: 5834,
-  //     status: 'unbooked',
-  //     maxOccupancy: 2,
-  //     booked: 0
-  //   },
-  //   {
-  //     x0: 0,
-  //     x1: 2,
-  //     y0: 4,
-  //     y1: 5,
-  //     z: 1,
-  //     roomNumber: 5835,
-  //     status: 'unbooked',
-  //     maxOccupancy: 2,
-  //     booked: 2
-  //   }
-  // ]
 ]
 
 const layout = {
   scene: {
-    xaxis: {
-      // title: ''
-      // showgrid: false,
-      // zeroline: false,
-      // showticklabels: false,
-      // showspikes: false
-    },
-    yaxis: {
-      // title: ''
-      // showgrid: false,
-      // zeroline: false,
-      // showticklabels: false,
-      // showspikes: false
-    },
-    zaxis: {
-      // title: ''
-      // showgrid: false,
-      // zeroline: false,
-      // showline: false,
-      // showticklabels: false,
-      // showspikes: false
-    },
+    // xaxis: {
+    //   title: '',
+    //   showgrid: false,
+    //   zeroline: false,
+    //   showticklabels: false,
+    //   showspikes: false
+    // },
+    // yaxis: {
+    //   title: '',
+    //   showgrid: false,
+    //   zeroline: false,
+    //   showticklabels: false,
+    //   showspikes: false
+    // },
+    // zaxis: {
+    //   title: '',
+    //   showgrid: false,
+    //   zeroline: false,
+    //   showline: false,
+    //   showticklabels: false,
+    //   showspikes: false
+    // },
     aspectmode: 'auto'
   },
   paper_bgcolor: 'rgba(0,0,0,0)',
   plot_bgcolor: 'rgba(0,0,0,0)'
 }
 
-const getStatus = (status, maxOccupancy, booked) => {
-  if (status === 'unbooked') {
-    return 'red'
+const getStatus = (maxOccupancy, booked) => {
+  const statusObj = {
+    status: '',
+    color: ''
+  }
+  if (booked === 0) {
+    statusObj.status = 'UNBOOKED'
+    statusObj.color = RED
   } else if (maxOccupancy === booked) {
-    return 'green'
-  } else return 'yellow'
+    statusObj.status = 'BOOKED'
+    statusObj.color = GREEN
+  } else {
+    statusObj.status = 'BOOKED'
+    statusObj.color = YELLOW
+  }
+  return statusObj
 }
 
 const getPolygonCoordinates = (
@@ -196,33 +198,33 @@ const getPolygonCoordinates = (
   y0,
   y1,
   l,
-  status,
+  roomNumber,
   maxOccupancy,
-  booked
+  booked,
+  customdata
 ) => {
+  const { color, status } = getStatus(maxOccupancy, booked)
   const h0 = y0 * l // Starting Height
   const h1 = y1 * l // Ending Height
   // Vertices of the polygon
   const v0 = x0 + h0
-  const v1 = x0 + x1 + h0
+  const v1 = x1 + h0
   const v2 = x0 + h1
-  const v3 = x0 + x1 + h1
-  const color = getStatus(status, maxOccupancy, booked)
+  const v3 = x1 + h1
+
+  // Set hover info
+  customdata[v1] = {
+    roomNumber,
+    status,
+    maxOccupancy,
+    booked
+  }
+
   return {
     i: [v0, v1],
     j: [v1, v2],
     k: [v2, v3],
-    facecolor: [color, color],
-    customdata: [
-      { status, maxOccupancy, booked },
-      { status, maxOccupancy, booked },
-      { status, maxOccupancy, booked },
-      { status, maxOccupancy, booked },
-      { status, maxOccupancy, booked },
-      { status, maxOccupancy, booked },
-      { status, maxOccupancy, booked },
-      { status, maxOccupancy, booked }
-    ]
+    facecolor: [color, color]
   }
 }
 
@@ -230,11 +232,18 @@ const createLevels = (data, level) => {
   const x = []
   const y = []
   const z = []
+  const customdata = []
   const length = 6
   for (let y1 = 0; y1 < length; y1++) {
     for (let x1 = 0; x1 < length; x1++) {
       x.push(x1)
       y.push(y1)
+      customdata.push({
+        roomNumber: '',
+        status: '',
+        maxOccupancy: '',
+        booked: ''
+      })
       z.push(level)
     }
   }
@@ -242,7 +251,6 @@ const createLevels = (data, level) => {
   const j = []
   const k = []
   const facecolor = []
-  const customdata = []
   data.forEach(point => {
     const coords = getPolygonCoordinates(
       point.x0,
@@ -250,19 +258,19 @@ const createLevels = (data, level) => {
       point.y0,
       point.y1,
       length,
-      point.status,
+      point.roomNumber,
       point.maxOccupancy,
-      point.booked
+      point.booked,
+      customdata
     )
     i.push(...coords.i)
     j.push(...coords.j)
     k.push(...coords.k)
     facecolor.push(...coords.facecolor)
-    customdata.push(...coords.customdata)
   })
 
   const hover = level === 0 ? hoverInfo : { hoverinfo: 'none' }
-
+  const opacity = level === 0 ? 1 : 0.5
   return {
     x,
     y,
@@ -270,10 +278,10 @@ const createLevels = (data, level) => {
     i,
     j,
     k,
+    opacity,
     facecolor,
     customdata,
     ...hover,
-    opacity: Math.random(),
     flatshading: true,
     type: 'mesh3d'
   }
@@ -292,7 +300,7 @@ const Playground = () => {
       layout={layout}
       revision={Math.random()}
       useResizeHandler={true}
-      style={{ width: '100%', height: '100%' }}
+      style={{ width: '100%', height: '900px' }}
       // onHover={hover => console.log(hover.points[0].data)}
     />
   )
