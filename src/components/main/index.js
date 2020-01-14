d2e6af56e5d7cbeb3504fbd9e59def07f87a449f
joneil@boostlabs.com <joneil@boostlabs.com>
2020-01-14 10:12:18 -0600

    push latest before pulling master

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
+
+// Project Imports
 import Header from './Header'
 import Filters from './filters'
+import SubFilters from './filters/sub-filters'
 import ShipViz from './ship-viz'
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
+`
+
+const Row = styled.div`
+  display: flex;
+`
+
+const ChartColumn = styled.div`
+  display: flex;
+  flex: 2;
+  flex-direction: column;
+  background-color: ${props => props.theme.black50};
+`
+
+const VizContainer = styled.div`
+  display: flex;
+  flex: 5;
   flex-direction: column;
 `
 
@@ -14,7 +34,13 @@ const Main = () => {
     <Container>
       <Header />
       <Filters />
-      <ShipViz />
+      <Row>
+        <ChartColumn />
+        <VizContainer>
+          <SubFilters />
+          <ShipViz />
+        </VizContainer>
+      </Row>
     </Container>
   )
 }
