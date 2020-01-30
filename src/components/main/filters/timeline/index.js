77a2e35d07e5cfd4eb0cee8336051703b855127e
joneil@boostlabs.com <joneil@boostlabs.com>
2020-01-30 10:35:13 -0600

    remove unused import

--- src/components/main/filters/timeline/index.js
+++ src/components/main/filters/timeline/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { useQuery } from '@apollo/client'
 import { store } from 'context/store'
 import styled from 'styled-components'
