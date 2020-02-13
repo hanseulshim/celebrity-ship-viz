b77a4c4c443788a032c91923e51d4b263da34b9b
joneil@boostlabs.com <joneil@boostlabs.com>
2020-02-13 11:56:08 -0600

    ship query

--- src/graphql/queries/ship.js
+++ src/graphql/queries/ship.js
@@ -1,8 +1,12 @@
 import gql from 'graphql-tag'
 
-export const GET_DECK_LIST = gql`
-  query deckList($shipId: Int, $sailingDateId: Int, $weeks: Int) {
-    deckList(shipId: $shipId, sailingDateId: $sailingDateId, weeks: $weeks) {
+export const GET_VISUAL_DECK_LIST = gql`
+  query deckVisualList($shipId: Int, $sailingDateId: Int, $weeks: Int) {
+    deckVisualList(
+      shipId: $shipId
+      sailingDateId: $sailingDateId
+      weeks: $weeks
+    ) {
       week
       date
     }
